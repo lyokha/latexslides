06c82bc19f47db940b3b6434e08ae1903804f55d
hpl@simula.no <hpl@simula.no@346a3b2e-b047-0410-b35a-f7c9bc4a2b6c>
2012-02-04 17:56:59 +0000

    updates

--- doc/clean.sh
+++ doc/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files="*.pyc *.aux *.dvi *.html *.log *.out doc.pdf doc.tex intro.tex *.nav intro.ps *.snm *.toc *.vrb _tmp* tmp* *~ doc.p.tex doc.gwiki doc.txt doc.idx"
+files="*.pyc *.aux *.dvi *.html *.log *.out doc.pdf doc.tex intro.tex *.nav intro.ps *.snm *.toc *.vrb _tmp* tmp* *~ doc.p.tex doc.gwiki doc.txt doc.idx tmp_*"
 echo "Removing:"
 ls $files 2> /dev/null
 rm -rf $files
